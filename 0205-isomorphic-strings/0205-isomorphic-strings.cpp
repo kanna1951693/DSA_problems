79be80405a870990bd1c59a42fc4c8f2d1e4a2be
Kunal1951 <kanna1951693@gmail.com>
2026-07-02 00:32:42 +0530

    Time: 8 ms (6.51%), Space: 9.4 MB (56.46%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -7,13 +7,11 @@ public:
             if(mpp.find(s[i])!=mpp.end()){
                 if(mpp[s[i]]!=t[i]) return false;
             }
-            mpp[s[i]]=t[i];
-        }
-        for(int i=0;i<s.size();i++){
             if(pmm.find(t[i])!=pmm.end()){
                 if(pmm[t[i]]!=s[i]) return false;
             }
             pmm[t[i]]=s[i];
+            mpp[s[i]]=t[i];
         }
         return true;
     }
